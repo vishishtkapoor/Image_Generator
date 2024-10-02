4f31c35763997606bdae64910d6f06c8bca6cd37
vishishtkapoor <vishishtkapoor2@gmail.com>
2024-10-03 01:55:40 +0530

    Add new feature

--- client/src/page/CreatePost.jsx
+++ client/src/page/CreatePost.jsx
@@ -28,7 +28,7 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch('https://dalle-arbb.onrender.com/api/v1/dalle', {
+        const response = await fetch('https://image-gen-3txh.onrender.com/api/v1/dalle', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
